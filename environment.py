e31128bff8d89e3388992c211ebc4392041e1358
RAVELLA VIKHIL <164651332+VikhilRavella@users.noreply.github.com>
2026-04-09 06:19:58 +0530

    Update environment.py

--- environment.py
+++ environment.py
@@ -1,8 +1,6 @@
 from __future__ import annotations
 
-import sys
-import os
-sys.path.insert(0, os.path.dirname(os.path.abspath(__file__)))
+
 
 from typing import Any, Dict, List, Optional, Tuple
 import pandas as pd
